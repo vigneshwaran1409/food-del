c2c5412589ff4f985ced8d46bec36ac30a1abea5
vigneshwaran1409 <mvigneshwaran1409@gmail.com>
2024-05-15 15:26:08 +0530

    Forntend update change

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -27,8 +27,8 @@ const Footer = () => {
         <div className="footer-content-right">
             <h2>GET IN TOUCH</h2>
             <ul>
-                <li>+1-212-456-7890</li>
-                <li>contact@tomato.com</li>
+                <li>9585947679</li>
+                <li>mvigneshwaran1409@gmail.com</li>
             </ul>
         </div>
       </div>
